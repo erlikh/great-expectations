ae76f43da7b914e683306908ea59f98a5465421e
Nick Erlikh <n.erlikh@gmail.com>
2015-10-12 13:48:01 +0300

    cleanup

--- test.js
+++ test.js
@@ -1,15 +1,13 @@
-var ge = require("./ge.js");
-
-var expect = ge.expect
+var ge = require("./ge.js")
+  , expect = ge.expect
   , eq = ge.eq
   , beGreaterThan = ge.beGreaterThan
 ;
 
+var tests = [
+  expect( 3 ).to( eq(3) ), // should return true
+  expect( 3 ).not.to( eq(3) ), // should return false
+  expect( 4 ).to( beGreaterThan(3) ) // should return true
+];
 
-
-var test1 = expect( 3 ).to( eq(3) ) // should return true
-  , test2 = expect( 3 ).not.to( eq(3) ) // should return false
-  , test3 = expect( 4 ).to( beGreaterThan(3) ) // should return true
-;
-
-console.log(test1, test2, test3);
+console.log("results", tests);
